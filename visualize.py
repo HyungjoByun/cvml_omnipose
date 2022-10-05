5b3b84f8e5ad09426d14270015bc7430ef59ea0d
HyungjoByun <hjomattehw9@gmail.com>
2022-10-05 08:51:45 +0000

    cyto

---
+++ visualize.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import tifffile as tif
+import os
+
+for name in os.listdir("/home/DataSet/Omnipose_test_rgb/masks/"):
+    image = tif.imread("/home/DataSet/Omnipose_test_rgb/masks/"+name)
+    plt.imsave("/home/cvml_omnipose/results/"+name+".png",image)
