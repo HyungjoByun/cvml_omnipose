76ad2fdc815cc31c02491552cb2b671141068a78
HyungjoByun <hjomatthew9@gmail.com>
2022-10-13 08:50:46 +0000

    raw_data

--- inference_cyto.sh
+++ inference_cyto.sh
@@ -1,4 +1,4 @@
-python -m my_omnipose --dir /home/DataSet/CellSeg/TuningSet \
---pretrained_model cellpose_residual_on_style_on_concatenation_off_Omnipose_rgb_2022_10_13_05_41_48.503318 \
---use_gpu --chan 2 --chan2 3 --diameter 0 --save_tif --no_npy \
+python -m my_omnipose --dir /home/DataSet/Omnipose_test_raw \
+--pretrained_model /home/DataSet/Omnipose_raw/models/cellpose_residual_on_style_on_concatenation_off_omni_Omnipose_raw_2022_10_13_08_44_31.955582 \
+--use_gpu --chan 2 --chan2 3 --save_tif --no_npy \
 --savedir ./outputs
--- train_cyto.sh
+++ train_cyto.sh
@@ -1,3 +1,3 @@
-python -m my_omnipose --train --use_gpu --dir /home/DataSet/Omnipose_rgb --mask_filter _masks --n_epochs 1 \
---pretrained_model cyto2_omni --train_size --chan 2 --chan2 3 --learning_rate 1e-6 --diameter 0 --batch_size 8 --RAdam --img_filter _img \
+python -m my_omnipose --train --use_gpu --dir /home/DataSet/Omnipose_raw --mask_filter _label --n_epochs 1 \
+--pretrained_model cyto2_omni --chan 2 --chan2 3 --learning_rate 1e-6 --diameter 0 --batch_size 8 --RAdam \
 --save_every 1 --min_train_masks 1
